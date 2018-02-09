74295c2cf0bd74d61ac1563bbca8391b3212e241
bergeronjulien <bergeron_julien@hotmail.com>
2018-02-09 11:18:59 -0500

    Removed EventEmitter from decleration

--- socket-io.service.ts
+++ socket-io.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter, Inject } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/share'; 
 
